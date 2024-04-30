7489364670d3accd5e3bb2169b7fa3be13aba14a
Kylian M <kylianm@MacBook-Air-de-Kylian.local>
2024-04-30 14:40:58 +0200

    H files created, for grid and pos of fruit

---
+++ grille.h
@@ -0,0 +1,20 @@
+#ifndef GRILLE_H_
+#define GRILLE_H_
+
+#include "pos.h"
+typedef struct grille {
+  int n;
+  int m;
+  char *** grid;
+  pos fruit;
+} grille;
+
+grille * grilleAllouer(int n, int m);
+void grilleVider(grille * g);
+void grilleTirageFruit(grille * g);
+void grilleRemplir(grille * g);
+void grilleDesallouer(grille * g);
+
+
+
+#endif 
---
+++ pos.h
@@ -0,0 +1,10 @@
+#ifndef POS_H_
+#define POS_H_
+
+typedef struct pos {
+  int x;
+  int y;
+} pos;
+
+
+#endif
